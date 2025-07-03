ca4629fe7aace02211abb20890fa1550342d4bb6
elmars-hub <martinsifeanyi247@gmail.com>
2025-07-03 16:26:36 +0100

    chore: update dependencies and add stack server app
    - Added @radix-ui/react-slot and @stackframe/stack to dependencies
    - Included next-themes for theme management
    - Created a new stack.tsx file to initialize StackServerApp with nextjs-cookie token store

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Home, Sprout } from "lucide-react";
+import Toogle from "./toogle";
+
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4 ">
+        <div className="flex items-center h-16 justify-between">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              🪴 Planventory
+            </Link>
+          </div>
+
+          {/* Navbar components */}
+          <div className="hidden md:flex items-center space-x-4">
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/plants">
+                <Sprout className="w-4 h-4" />
+                <span className="hidden lg:inline">Plant</span>
+              </Link>
+            </Button>
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/">
+                <Home className="w-4 h-4" />
+                <span className="hidden lg:inline ">Home</span>
+              </Link>
+            </Button>
+
+            <Toogle />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
