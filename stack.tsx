6959d9942c506b9ae1d09e021b015f9805cedbb1
elmars-hub <martinsifeanyi247@gmail.com>
2025-07-11 06:28:29 +0100

    fix

--- stack.tsx
+++ stack.tsx
@@ -1,6 +1,5 @@
 import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
-  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
   tokenStore: "nextjs-cookie",
 });
