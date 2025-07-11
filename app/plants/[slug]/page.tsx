2503ace142c6fba799621fbb89fef58fb65b8a2b
elmars-hub <martinsifeanyi247@gmail.com>
2025-07-11 05:23:33 +0100

    Revert "fix: build errors"
    This reverts commit eef9f1ef97133ea71d4c6215feefc10d49d2c283.

--- app/plants/[slug]/page.tsx
+++ app/plants/[slug]/page.tsx
@@ -5,6 +5,7 @@ import PlantCard from "./PlantCard";
 import { getPlantsById } from "@/app/actions/plant.action";
 import { SignIn } from "@stackframe/stack";
 
+<<<<<<< HEAD
 export default function Page({ params }: { params: { slug: string } }) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [user, setUser] = useState<any>(null);
@@ -65,6 +66,39 @@ export default function Page({ params }: { params: { slug: string } }) {
       </div>
     );
   }
+=======
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const [id] = params.slug.split("--");
+  const plant = await getPlantsById(id);
+
+  return {
+    title: plant ? plant.name : "Plant Details",
+    description: plant ? plant.description : "Plant details page ",
+  };
+}
+
+async function Page({ params }: { params: { slug: string } }) {
+  const user = await stackServerApp.getUser();
+  const [id] = params.slug.split("--");
+  const plant = await getPlantsById(id);
+
+  if (!user) {
+    return <SignIn />;
+  }
+
+  return (
+    <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-full">
+        <PlantCard plant={plant} />
+      </div>
+    </div>
+  );
+}
+>>>>>>> parent of eef9f1e (fix: build errors)
 
   if (!user) {
     return <SignIn />;
